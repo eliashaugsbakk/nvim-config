1d7583870a631b00737055f917a622ddda11add5
Elias Haugsbakk <elias.haugsbakk@gmail.com>
2026-03-11 20:21:08 +0100

    update init.lua to include rust-analyzer lsp

--- init.lua
+++ init.lua
@@ -606,8 +606,8 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
-        --
+        rust_analyzer = {},
+
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
